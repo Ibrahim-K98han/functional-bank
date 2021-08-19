62d668de64eb59868d65d19e9647b21d1a53114d
Ibrahim Khan <aitvetibrahim@gmail.com>
2021-08-19 08:42:54 +0600

    updated

--- js/banking.js
+++ js/banking.js
@@ -17,4 +17,26 @@ document.getElementById('deposit-button').addEventListener('click', function () 
 
     //clear input filed
     depositInput.value = '';
-})
+});
+
+//handle withdraw button
+document.getElementById('withdraw-button').addEventListener('click', function () {
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawAmountText = withdrawInput.value;
+    const withdrawAmount = parseFloat(withdrawAmountText);
+
+    //update withdraw total
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdrawTotalText = withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawTotalText);
+    withdrawTotal.innerText = previousWithdrawTotal + withdrawAmount;
+
+    //update balance after withdraw
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previsousBalanceTotal = parseFloat(balanceTotalText);
+    balanceTotal.innerText = previsousBalanceTotal - withdrawAmount;
+
+    //cler withdraw input filed
+    withdrawInput.value = '';
+});
